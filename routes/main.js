f40727add41f1900860403707113f507e6875bee
Perry M <pgmerilo@gmail.com>
2022-06-22 16:14:10 +0800

    FIXING ROUTES
    moved routes folder to root
    function to set layout by route in router files
    remove if-else from layout handlebars

---
+++ routes/main.js
@@ -0,0 +1,219 @@
+const express = require('express');
+const router = express.Router();
+const flashMessage = require('../views/helpers/messenger');
+const sequelizeUser = require("../config/DBConfig");
+const { serializeUser } = require('passport');
+const User = require("../models/User")
+const Ticket = require("../models/Ticket")
+const Feedback = require("../models/Feedback")
+const Product = require("../models/Product")
+const ensureAuthenticated = require("../views/helpers/auth");
+
+
+router.use((req, res, next) => {
+    res.locals.path = req.baseUrl;
+    console.log(req.baseUrl);
+    //Checks url for normal users and admin
+    next();
+});
+
+router.all('/*', function (req, res, next) {
+    req.app.locals.layout = 'main'; // set your layout here
+    next(); // pass control to the next handler
+});
+
+
+router.get('/', (req,res) => {
+    res.render("index")
+})
+
+router.get('/RewardsPage', (req,res) => {
+    res.render("rewards")
+})
+
+router.get('/CustomerService', (req,res) => {
+    res.render("customerservice")
+})
+
+
+router.get('/profile',ensureAuthenticated, (req,res) => {
+    
+    res.render("profile")
+})
+
+router.post('/flash', (req, res) => {
+	const message = 'This is an important message';
+	const error = 'This is an error message';
+	const error2 = 'This is the second error message';
+
+    // req.flash('message', message);
+    // req.flash('error', error);
+    // req.flash('error', error2);
+
+    flashMessage(res, 'success', message);
+    flashMessage(res, 'info', message);
+    flashMessage(res, 'error', error);
+    flashMessage(res, 'error', error2, 'fas fa-sign-in-alt', true);
+
+	res.redirect('/');
+});
+
+router.get('/editProfile',ensureAuthenticated, (req,res) => {
+    res.render("editProfile.handlebars")
+})
+
+router.get('/changePassword',ensureAuthenticated, (req,res) => {
+    res.render("userEditPassword.handlebars")
+})
+
+router.get('/myOrders',ensureAuthenticated, (req,res) => {
+    res.render("myOrders.handlebars")
+})
+
+router.get('/shoppingCart', (req,res) => {
+    res.render("shoppingCart.handlebars")
+})
+
+router.get('/otherSupport', (req,res) => {
+    res.render("qnaPages/otherSupport.handlebars")
+})
+
+router.get('/gettingStarted', (req,res) => {
+    res.render("qnaPages/gettingStarted.handlebars")
+})
+
+router.get('/myAccountQNA', (req,res) => {
+    res.render("qnaPages/myAccountQNA.handlebars")
+})
+
+router.get('/payment&shippingQNA', (req,res) => {
+    res.render("qnaPages/payment&shippingQNA.handlebars")
+})
+router.get('/troubleshootingQNA', (req,res) => {
+    res.render("qnaPages/troubleshootingQNA.handlebars")
+})
+
+router.get('/rewards&offersQNA', (req,res) => {
+    res.render("qnaPages/rewards&offersQNA.handlebars")
+})
+
+
+router.get('/messages',ensureAuthenticated, (req,res) => {
+    res.render("messages.handlebars")
+})
+router.get('/feedback',ensureAuthenticated, (req,res) => {
+    res.render("feedback.handlebars")
+})
+router.get('/tickets',ensureAuthenticated, (req,res) => {
+    res.render("ticket.handlebars")
+})
+
+router.post('/tickets',ensureAuthenticated, async function (req,res) {
+    let date_ob = new Date();
+    // current date
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    // current hours
+    let hours = date_ob.getHours();
+    // current minutes
+    let minutes = date_ob.getMinutes();
+
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+    let { title, urgency, description} = req.body;
+    try{
+        await Ticket.create({
+            title: req.body.title,
+            urgency: req.body.urgency,
+            description: req.body.description,
+            pendingStatus: "Pending",
+            dateAdded: year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds,
+            owner: "Hi",
+            ownerID: 1
+  
+          });
+          flashMessage(res,"success",'Ticket Sent Successfully');
+          res.redirect("/tickets")
+    }catch(e){
+         console.log(e)
+         res.redirect("/tickets")
+    }
+})
+
+router.post('/feedback',ensureAuthenticated, async function (req,res) {
+    let date_ob = new Date();
+    // current date
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    // current hours
+    let hours = date_ob.getHours();
+    // current minutes
+    let minutes = date_ob.getMinutes();
+
+    // current seconds
+    let seconds = date_ob.getSeconds();
+
+    let { title, favouriteThing,leastFavouriteThing,description,remarks,rating } = req.body;
+    try{
+        await Feedback.create({
+            title: req.body.title,
+            favouriteThing: req.body.favouriteThing,
+            leastFavouriteThing: req.body.leastFavouriteThing,
+            description: req.body.description,
+            remarks: req.body.remarks,
+            rating: req.body.rating,
+            dateAdded: year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds,
+            owner: "Hi",
+            ownerID: 1
+  
+          });
+          flashMessage(res,"success",'Feedback Sent Successfully');
+          res.redirect("/feedback")
+    }catch(e){
+         console.log(e)
+         res.redirect("/feedback")
+    }
+})
+
+router.get('/addProduct',ensureAuthenticated, (req,res) => {
+    res.render("addProduct.handlebars")
+})
+
+router.post('/addProduct',ensureAuthenticated, async function (req,res) {
+    let { sku,name,description,price,quantity,category } = req.body;
+    try{
+        await Product.create({
+            sku: req.body.sku,
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            category: req.body.category
+            
+  
+          });
+          flashMessage(res,"success",'Product Added Successfully');
+          res.redirect("/addProduct")
+    }catch(e){
+         console.log(e)
+         res.redirect("/addProduct")
+    }
+})
+
+module.exports = router;
+
