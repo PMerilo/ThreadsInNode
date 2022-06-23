235bdc35082a165e53177d3e183a16c1a309c169
bob <ezellboyss@gmail.com>
2022-06-23 20:55:46 +0800

    user edit profile works

--- views/routes/user.js
+++ views/routes/user.js
@@ -17,86 +17,105 @@ router.use((req, res, next) => {
 
 
 router.get('/login', (req, res) => {
-    res.render('user/login');
+  res.render('user/login');
 });
 
 router.get('/register', (req, res) => {
-    res.render('user/register');
+  res.render('user/register');
 });
 
 
 router.post('/register', async function (req, res) {
 
-    let { name, email, password, password2 } = req.body;
+  let { name, email, password, password2 } = req.body;
 
-    let isValid = true;
-    if (password.length < 6) {
-        flashMessage(res, 'error', 'Password must be at least 6 char-acters');
-        isValid = false;
-    }
-    if (password != password2) {
-        flashMessage(res, 'error', 'Passwords do not match');
-        isValid = false;
-    }
-    
-    try {
-        if (
-          (await User.findOne({
-            where: { name: req.body.name },
-          })) ||
-          (await User.findOne({ where: { email: req.body.name } }))
-        ) {
-          flashMessage(res, 'error', 'Name or email is not unique');
-          console.log("Name or email is not unique");
-  
-          return res.redirect("/register");
-        }
-        await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-          gender: req.body.gender,
-          phoneNumber: req.body.phoneNumber
+  let isValid = true;
+  if (password.length < 6) {
+    flashMessage(res, 'error', 'Password must be at least 6 char-acters');
+    isValid = false;
+  }
+  if (password != password2) {
+    flashMessage(res, 'error', 'Passwords do not match');
+    isValid = false;
+  }
 
-        });
-        flashMessage(res, 'success', email + ' registered successfully');
-        res.redirect("/login");
-      } catch (e) {
-        console.log(e);
-        res.redirect("/register");
-      }
+  try {
+    if (
+      (await User.findOne({
+        where: { name: req.body.name },
+      })) ||
+      (await User.findOne({ where: { email: req.body.name } }))
+    ) {
+      flashMessage(res, 'error', 'Name or email is not unique');
+      console.log("Name or email is not unique");
+
+      return res.redirect("/register");
+    }
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      gender: req.body.gender,
+      phoneNumber: req.body.phoneNumber
+
     });
+    flashMessage(res, 'success', email + ' registered successfully');
+    res.redirect("/login");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/register");
+  }
+});
 
-    router.post('/login', (req, res, next) => {
-      passport.authenticate('local', {
-          // Success redirect URL
-          successRedirect: '/profile',
-          // Failure redirect URL 
-          failureRedirect: '/login',
-          /* Setting the failureFlash option to true instructs Passport to flash 
-          an error message using the message given by the strategy's verify callback.
-          When a failure occur passport passes the message object as error */
-          failureFlash: true
-          
-          
-      })(req, res, next);
-      
-  })
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    // Success redirect URL
+    successRedirect: '/profile',
+    // Failure redirect URL 
+    failureRedirect: '/login',
+    /* Setting the failureFlash option to true instructs Passport to flash 
+    an error message using the message given by the strategy's verify callback.
+    When a failure occur passport passes the message object as error */
+    failureFlash: true
+
+
+  })(req, res, next);
+
+})
+
+router.post('/editProfile', ensureAuthenticated, async (req, res) => {
+  userr = await User.findOne({ where: { id: req.user.id } })
+  if ((await User.findOne({ where: { email: req.body.email } })) && userr.email != req.body.email) {
+    flashMessage(res, 'error', 'This email has already been registered');
+    return res.redirect("/editProfile");
+  }
+  if ((await User.findOne({ where: { name: req.body.name } })) && userr.name != req.body.name) {
+    flashMessage(res, 'error', 'This name has already been registered');
+    return res.redirect("/editProfile");
+  }
+  let name = req.body.name;
+  let email = req.body.email;
+  let phoneNumber = req.body.phoneNumber;
+  let gender = req.body.gender;
+  User.update({ name, email, phoneNumber, gender }, { where: { id: req.user.id } })
+  res.redirect("/profile");
+
+})
 
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        
-        return next()
-    }
+  if (req.isAuthenticated()) {
 
-    res.redirect("/profile")
-    }
+    return next()
+  }
 
-router.get('/logout',ensureAuthenticated, (req, res) => {
-    const message = 'You Have Logged out';
-    flashMessage(res, 'success', message);
-    req.logout();
-    res.redirect('/');
+  res.redirect("/profile")
+}
+
+router.get('/logout', ensureAuthenticated, (req, res) => {
+  const message = 'You Have Logged out';
+  flashMessage(res, 'success', message);
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
