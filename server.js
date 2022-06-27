6256cbd7227b81b14ce77736a6c4482ccb84447d
Perry M <pgmerilo@gmail.com>
2022-06-27 15:29:44 +0800

    Merge Fixes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const session = require('express-session');
 const main = require("./routes/main")
 const user = require("./routes/user")
 const admin = require("./routes/admin")
-const seller= require("./routes/seller")
+const seller = require("./routes/seller")
+const services = require("./routes/services")
 const datapipeline = require("./routes/datapipeline")
 const bodypassword = require('body-parser')
 const GoogleAuth = require("./config/passportGoogleAuth")
@@ -99,6 +100,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", main)
 app.use("/", user)
 app.use("/admin",admin)
+app.use("/services",services)
 app.use("/seller",seller)
 app.use("/datapipeline",datapipeline)
 
